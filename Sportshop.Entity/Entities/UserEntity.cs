bb383c774f40be255ea488489bcd0122301f52c5
karaokesound <karaokesound24@gmail.com>
2023-08-30 10:11:36 +0200

    Added a constructor to the UserEntity class, added first migration (userentity) with seeded users

--- Sportshop.Entity/Entities/UserEntity.cs
+++ Sportshop.Entity/Entities/UserEntity.cs
@@ -4,13 +4,13 @@
     {
         public Guid Id { get; set; }
 
-        public string Username { get; set; } = null!;
+        public string Username { get; set; }
 
         public string FirstName { get; set; } = string.Empty;
 
         public string LastName { get; set; } = string.Empty;
 
-        public string City { get; set; } = null!;
+        public string City { get; set; }
 
         public int Age { get; set; }
 
@@ -23,5 +23,14 @@
         public DateTime TokenCreated { get; set; }
 
         public DateTime TokenExpires { get; set; }
+
+        public UserEntity(string username, string city, int age)
+        {
+            // Id property is adding automatically by the EF (set in the UserEntityConfiguration.cs)
+            Id = Guid.NewGuid();
+            Username = username;
+            City = city;
+            Age = age;
+        }
     }
 }
--- Sportshop.Persistence/Context/SportshopDbContext.cs
+++ Sportshop.Persistence/Context/SportshopDbContext.cs
@@ -11,5 +11,66 @@ namespace Sportshop.Persistence.Context
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>()
+                .HasData(
+                new UserEntity("User1", "City1", 21)
+                {
+                    FirstName = "Jan",
+                    LastName = "Nowak",
+                },
+                new UserEntity("User2", "City2", 22)
+                {
+                    FirstName = "Andrzej",
+                    LastName = "Kowalski",
+                },
+                new UserEntity("User3", "City3", 23)
+                {
+                    FirstName = "Piotr",
+                    LastName = "Kowalczyk",
+                },
+                new UserEntity("User4", "City4", 24)
+                {
+                    FirstName = "Kamil",
+                    LastName = "Grabak",
+                },
+                new UserEntity("User5", "City5", 25)
+                {
+                    FirstName = "Leon",
+                    LastName = "Ziutkiewicz",
+                },
+                new UserEntity("User6", "City6", 26)
+                {
+                    FirstName = "Karol",
+                    LastName = "Strasburger",
+                },
+                new UserEntity("User7", "City7", 27)
+                {
+                    FirstName = "Andrzej",
+                    LastName = "Kipik",
+                },
+                new UserEntity("User8", "City8", 28)
+                {
+                    FirstName = "Marzena",
+                    LastName = "Bogacz",
+                },
+                new UserEntity("User9", "City9", 29)
+                {
+                    FirstName = "Joanna",
+                    LastName = "Leśniewska",
+                },
+                new UserEntity("User10", "City10", 30)
+                {
+                    FirstName = "Weronika",
+                    LastName = "Kalinka",
+                },
+                new UserEntity("User11", "City11", 31),
+                new UserEntity("User12", "City12", 32)
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
